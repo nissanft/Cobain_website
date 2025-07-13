be7ebee64731d73666d454a3b0946f8ab1eb0f86
Nissa <nissanurfitri005@gmail.com>
2025-07-13 21:41:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,16 +23,16 @@ fitur = st.sidebar.selectbox("🔬 Chem Elements", ["Element", "Periodic Table"]
 
 """Konten berdasarkan pilihan menu"""
 def tampilkan_menu(menu):
-if selected_menu == "About":
+if menu == "About":
     st.header("ℹ️ About")
     st.write("This website is created to fulfill a project assignment. Chemistry is fascinating!")
-elif selected_menu == "Contact":
+elif menu == "Contact":
     st.header("📞 Contact")
     st.write("Contact us: LPK's Group – we’re always up for a good science chat.")
 
 """For Sidebar next selectbox 1"""
 def tampilkan_fitur(fitur):
-if selected_fitur == "Periodic Table":
+if fitur == "Periodic Table":
     st.header("🧪 Periodic Table")
     st.write("Here's The Periodic Table – all elements")
     
